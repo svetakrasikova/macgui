756fefe6b2e7a398aced371f7f548faad547e1cf
Sveta Krasikova <svetakrasikova@me.com>
2019-01-31 17:31:42 -0800

    Initial Commit

---
+++ macgui/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macgui
+//
+//  Created by Svetlana Krasikova on 1/31/19.
+//  Copyright © 2019 Svetlana Krasikova. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
