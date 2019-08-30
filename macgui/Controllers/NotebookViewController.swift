9d1726b6f931033ee5185a98984749ba142f035c
Sveta Krasikova <svetakrasikova@me.com>
2019-08-30 11:22:32 -0700

    Add notebook view controller.

---
+++ macgui/Controllers/NotebookViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NotebookViewController.swift
+//  macgui
+//
+//  Created by Svetlana Krasikova on 8/30/19.
+//  Copyright © 2019 Svetlana Krasikova. All rights reserved.
+//
+
+import Cocoa
+
+class NotebookViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
