0ee264a0a5eaf28cdef72d134495c00b6bb9493f
Sveta Krasikova <svetakrasikova@me.com>
2019-10-02 08:35:36 -0700

    Subclass DataMatrixViewController.

---
+++ macgui/Controllers/ReadData/DataMatrixViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DataMatrixViewController.swift
+//  macgui
+//
+//  Created by Svetlana Krasikova on 10/1/19.
+//  Copyright © 2019 Svetlana Krasikova. All rights reserved.
+//
+
+import Cocoa
+
+class DataMatrixViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
