feb7135d9cd5f5bf53328ed1472e0b04bb09e5d7
John Huelsenbeck <johnh@berkeley.edu>
2019-11-05 12:15:33 -0800

    Added (a nonfunctioning) function for converting C++ data matrices to Swift data matrices.

---
+++ macgui/Model/Data Matrix/DataMatrixContainer.swift
@@ -0,0 +1,15 @@
+//
+//  DataMatrixContainer.swift
+//  macgui
+//
+//  Created by John Huelsenbeck on 11/5/19.
+//  Copyright © 2019 Svetlana Krasikova. All rights reserved.
+//
+
+import Foundation
+
+@objc class DataMatrixContainer : NSObject {
+
+    var dataMatrices: [DataMatrix]?
+
+}
