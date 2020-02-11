7340093d1402a37103ba922a483d750ae15e8191
Sveta Krasikova <svetakrasikova@me.com>
2020-02-11 15:41:30 -0800

    Intialize palettItems property from json string read from the core.

--- macgui/Views/Main Canvas Object/Canvas Tool/Tool Objects/Connectable Tools/Model.swift
+++ macgui/Views/Main Canvas Object/Canvas Tool/Tool Objects/Connectable Tools/Model.swift
@@ -9,7 +9,13 @@
 import Cocoa
 
 class Model: DataTool {
-   
+    
+    let revbayesBridge =  (NSApp.delegate as! AppDelegate).coreBridge
+    
+    var palettItems: [PalettItem] = []
+    
+    
+    
     override init(name: String, frameOnCanvas: NSRect, analysis: Analysis) {
         super.init(name: name, frameOnCanvas: frameOnCanvas, analysis: analysis)
         
@@ -18,9 +24,33 @@ class Model: DataTool {
         let purple = Connector(color:ConnectorType.purple)
         self.inlets = [green]
         self.outlets = [purple]
+//        try! initPalettItemsFromCore()
+//        print ("PalettItems contain \(palettItems.count) elements")
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
+    
+    func initPalettItemsFromCore() throws {
+        
+        guard let jsonStringArray = revbayesBridge.getPalletItems() as? [String]
+            else {
+                throw DataToolError.readError
+        }
+        
+        let palettItemsData: [Data] = JsonCoreBridge(jsonArray: jsonStringArray).encodeJsonStringArray()
+        
+        for data in palettItemsData {
+            
+            do {
+                let newPalettItem = try JSONDecoder().decode(PalettItem.self, from: data)
+                self.palettItems.append(newPalettItem)
+            } catch  {
+                throw ReadDataError.dataDecodingError
+            }
+        }
+    }
+    
+    
 }
