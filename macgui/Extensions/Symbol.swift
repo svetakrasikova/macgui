8c167755f1e7bf7123b130b808079adc839d8ac9
Sveta Krasikova <svetakrasikova@me.com>
2020-03-23 15:29:39 -0700

    Simplify Symbol enum.

---
+++ macgui/Extensions/Symbol.swift
@@ -0,0 +1,14 @@
+//
+//  Label.swift
+//  macgui
+//
+//  Created by Svetlana Krasikova on 3/22/20.
+//  Copyright © 2020 Svetlana Krasikova. All rights reserved.
+//
+
+import Cocoa
+
+enum Symbol: String {
+    case doubleStruckCapitalN = "\u{2115}"
+    case doubleStruckCapitalR = "\u{211D}"
+}
