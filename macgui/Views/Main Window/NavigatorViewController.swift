46b04ac19aa8e4ae77eb0cbeb3400e17afa6c70a
Sveta Krasikova <svetakrasikova@me.com>
2020-03-30 20:59:58 -0700

    Implement table view data source and data delegate methods. Remove array controller.

--- macgui/Views/Main Window/NavigatorViewController.swift
+++ macgui/Views/Main Window/NavigatorViewController.swift
@@ -10,96 +10,97 @@ import Cocoa
 
 protocol NavigatorViewControllerDelegate: class {
     func navigatorViewController(viewController: NavigatorViewController,
-                                  selectedAnalysis: Analysis?) -> Void
+                                 selectedAnalysis: Analysis?) -> Void
+}
+
+
+class NavigatorViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource, NSWindowDelegate {
+    
+    
+    @IBOutlet weak var actionButton: NSPopUpButton!
+    @IBOutlet weak var analysesTableView: NSTableView!
+    
+    override var representedObject: Any? {
+        didSet {
+            analysesTableView.reloadData()
+        }
     }
     
-
-class NavigatorViewController: NSViewController {
-    
-
-    @IBOutlet weak var actionButton: NSPopUpButton!
-    @IBOutlet weak var arrayController : NSArrayController!
-    @IBOutlet weak var analysesTableView: NSTableView!
-   
     var analyses: [Analysis] {
-        let analyses = (self.representedObject as! Document).dataSource?.analyses
-        assert(analyses != nil, "Unable to find the document for this view controller.")
-        return analyses!
+        get {
+            guard let document = self.representedObject as? Document, let analyses = document.dataSource?.analyses else { return [] }
+            return analyses
+        }
+        set {
+            guard let document = self.representedObject as? Document else { return }
+            document.dataSource?.analyses = newValue
+        }
     }
     weak var delegate: NavigatorViewControllerDelegate?
     
-    @IBAction func actionButtonClicked(_ sender: NSPopUpButton) {
-        switch sender.selectedItem?.tag {
-        case 1:
-            addAnalysis()
-        case 2:
-            if let selectedForRemoval = arrayController.selectedObjects {
-                arrayController.removeSelectedAnalyses(toRemove: selectedForRemoval as! [Analysis])
-                if analyses.isEmpty {
-                    addAnalysis()
-                }
-            }
-        case 3:
-            if let selectedAnalysis = arrayController.selectedObjects.first as! Analysis? {
-                copyAnalysis(from: selectedAnalysis)
-            }
-        default:
-            print("Switch case error")
-            
+    
+    @IBAction func removeAnalysisClicked(_ sender: NSPopUpButton) {
+        let selectedRow = analysesTableView.selectedRow
+        if selectedRow == -1 { return }
+        analyses.remove(at: selectedRow)
+        let indexSet = IndexSet(integer: selectedRow)
+        analysesTableView.removeRows(at: indexSet, withAnimation:.effectFade)
+        if analyses.isEmpty {
+            let analysis = Analysis(name: getUniqueAnalysisName(prefix: "untitled analysis", isCopy: false))
+            addAnalysis(analysis, row: 0)
+        } else {
+            analysesTableView.selectRowIndexes(IndexSet(integer: selectedRow-1), byExtendingSelection: false)
         }
-        setSelectedAnalysisToActive()
+    }
+    
+    @IBAction func addAnalysisClicked(_ sender: NSPopUpButton) {
+        let analysis = Analysis(name: getUniqueAnalysisName(prefix: "untitled analysis", isCopy: false))
+        var selectedRow = analysesTableView.selectedRow
+        if selectedRow == -1 { selectedRow = analyses.count - 1}
+        addAnalysis(analysis, row: selectedRow+1)
         
     }
-   
     
-    /**
-     Trigger a call on the delegate to update the detail view if a new analysis is selected in the table view
-     */
-    @IBAction func selectAnalysis(sender: AnyObject){
-        let selectedAnalysis = arrayController.selectedObjects.first as! Analysis?
-        delegate?.navigatorViewController(viewController: self, selectedAnalysis: selectedAnalysis)
+    @IBAction func copyAnalysisClicked(_ sender: NSPopUpButton) {
+        let selectedRow = analysesTableView.selectedRow
+        let selectedAnalysis = analyses[selectedRow]
+        if let analysis = selectedAnalysis.copy() as? Analysis {
+            let prefix = selectedAnalysis.name
+            analysis.name = getUniqueAnalysisName(prefix: prefix, isCopy: true)
+            addAnalysis(analysis, row: selectedRow + 1)
+        }
+        
     }
     
-    /**
-     Trigger a call on the delegate to update the detail view if an analysis is added or deleted
-     */
-    
-    func setSelectedAnalysisToActive(){
-        let index: IndexSet = [arrayController.selectionIndex]
-        analysesTableView.selectRowIndexes(index, byExtendingSelection: false)
-        let selectedAnalysis = arrayController.selectedObjects.first as! Analysis?
-        delegate?.navigatorViewController(viewController: self, selectedAnalysis: selectedAnalysis)
+    func addAnalysis(_ analysis: Analysis, row: Int) {
+        analyses.insert(analysis, at: row)
+        let indexSet = IndexSet(integer: row)
+        analysesTableView.insertRows(at: indexSet, withAnimation: .slideDown)
+        analysesTableView.selectRowIndexes(indexSet, byExtendingSelection: false)
     }
     
-    /**
-     Add a new empty analysis
-     */
-    func addAnalysis() {
-        let analysis = Analysis(name: getUniqueAnalysisName(prefix: "untitled analysis", isCopy: false))
-        arrayController.addObject(analysis)
-    }
-    
-    /**
-     Copy an analysis
-     */
-    func copyAnalysis(from: Analysis) {
-        let analysis = from.copy() as! Analysis
-        let prefix = from.name
-        analysis.name = getUniqueAnalysisName(prefix: prefix, isCopy: true)
-        arrayController.addObject(analysis)
-    }
-    
- 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let indices = IndexSet([0])
-        analysesTableView.selectRowIndexes(indices, byExtendingSelection: false)
     }
     
-    override func viewDidAppear() {
-        super.viewDidAppear()
-        setSelectedAnalysisToActive()
-
+    
+    // MARK: - NSTableViewDataSource
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return analyses.count
+    }
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        return analyses[row]
+    }
+    
+    // MARK: - NSTableViewDelegate
+    
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        let row = analysesTableView.selectedRow
+        if row >= 0  && analysesTableView.selectedRowIndexes.count == 1  {
+            delegate?.navigatorViewController(viewController: self, selectedAnalysis: analyses[row])
+        }
     }
 }
 
