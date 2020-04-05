626e3ba1f8376906686b832a11ac2e0c09a8248b
Sveta Krasikova <svetakrasikova@me.com>
2020-04-04 18:04:47 -0700

    Delete analyses by pressing delete key. Add alert modal when an analyses is deleted. Add deletion of multiple rows.

--- macgui/Views/Main Window/NavigatorViewController.swift
+++ macgui/Views/Main Window/NavigatorViewController.swift
@@ -40,17 +40,11 @@ class NavigatorViewController: NSViewController, NSTableViewDelegate, NSTableVie
     
     
     @IBAction func removeAnalysisClicked(_ sender: NSPopUpButton) {
-        let selectedRow = analysesTableView.selectedRow
-        if selectedRow == -1 { return }
-        analyses.remove(at: selectedRow)
-        let indexSet = IndexSet(integer: selectedRow)
-        analysesTableView.removeRows(at: indexSet, withAnimation:.effectFade)
-        if analyses.isEmpty {
-            let analysis = Analysis(name: getUniqueAnalysisName(prefix: "untitled analysis", isCopy: false))
-            addAnalysis(analysis, row: 0)
-        } else {
-            analysesTableView.selectRowIndexes(IndexSet(integer: selectedRow-1), byExtendingSelection: false)
-        }
+        removeSelectedRowsFromAnalyses()
+    }
+    
+    @IBAction func delete(_ sender: AnyObject) {
+        removeSelectedRowsFromAnalyses()
     }
     
     @IBAction func addAnalysisClicked(_ sender: NSPopUpButton) {
@@ -112,6 +106,116 @@ class NavigatorViewController: NSViewController, NSTableViewDelegate, NSTableVie
         let row = analysesTableView.selectedRow
         if row >= 0  && analysesTableView.selectedRowIndexes.count == 1  {
             delegate?.navigatorViewController(viewController: self, selectedAnalysis: analyses[row])
+        }
+    }
+}
+
+
+extension NavigatorViewController {
+    
+    /**
+     Create a unique default analysis name starting with prefix followed by the next available numerical identifier
+     - parameter prefix: the initial substring of the returned name
+     - returns: A unique name for an analysis
+     */
+    
+    
+    func getUniqueAnalysisName(prefix: String, isCopy: Bool) -> String {
+        var prefixUntilCopy = prefix
+        var defaultNameIndices = [Int](repeating: 0, count: analyses.count)
+        if let index = prefix.range(of: " copy")?.lowerBound{
+            prefixUntilCopy = String(prefix.prefix(upTo: index))
+        }
+        for analysis in analyses{
+            if analysis.name.starts(with: prefixUntilCopy){
+                if !isCopy {
+                    if analysis.name == prefix {
+                        defaultNameIndices[0] = 1
+                    } else {
+                        if  analysis.name.range(of: "copy") == nil {
+                            let numericalIdentifierIndex = (analysis.name.components(separatedBy: " ").count)-1
+                            if let index = Int(analysis.name.components(separatedBy: " ")[numericalIdentifierIndex]){
+                                if index >= defaultNameIndices.count {
+                                    defaultNameIndices.insert(1, at: defaultNameIndices.count)
+                                } else {
+                                    defaultNameIndices[index] = 1
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    if analysis.name.hasSuffix("copy") ||  analysis.name.range(of: "copy") == nil {
+                        defaultNameIndices[0] = 1
+                    } else {
+                        if  analysis.name.range(of: "copy") != nil {
+                            let numericalIdentifierIndex = (analysis.name.components(separatedBy: " ").count)-1
+                            if let index = Int(analysis.name.components(separatedBy: " ")[numericalIdentifierIndex]){
+                                if index >= defaultNameIndices.count {
+                                    defaultNameIndices.insert(1, at: defaultNameIndices.count)
+                                } else {
+                                    defaultNameIndices[index] = 1
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if defaultNameIndices.isEmpty || defaultNameIndices[0] == 0 {
+            return isCopy ? "\(prefixUntilCopy) copy" : prefix
+        }
+        for (i,v) in defaultNameIndices.enumerated(){
+            if v == 0 {
+                return isCopy ? "\(prefixUntilCopy) copy \(i)": "\(prefix) \(i)"
+            }
+        }
+        return isCopy ? "\(prefixUntilCopy) copy \(defaultNameIndices.count)": "\(prefix) \(defaultNameIndices.count)"
+    }
+}
+
+extension NavigatorViewController {
+ 
+    func removeSelectedRowsFromAnalyses(){
+        let selectedRow = analysesTableView.selectedRow
+        if selectedRow == -1 { return }
+        let indexSet = analysesTableView.selectedRowIndexes
+        let count = analysesTableView.selectedRowIndexes.count
+        
+        let alert = NSAlert()
+        alert.messageText = "Warning: Delete Analysis"
+        alert.addButton(withTitle: "Delete")
+        alert.addButton(withTitle: "Cancel")
+        
+        if count > 1 {
+            alert.informativeText = "You are about to delete \(count) analyses, with all their associated information. Do you wish to continue with this operation?"
+        } else {
+            alert.informativeText = "You are about to delete an analysis, with all of its associated information. Do you wish to continue with this operation?"
+        }
+        
+        let result = alert.runModal()
+        
+        switch result {
+        case NSApplication.ModalResponse.alertFirstButtonReturn:
+            var iteration = 0
+            for index in indexSet {
+                analyses.remove(at: index-iteration)
+                iteration += 1
+            }
+            analysesTableView.removeRows(at: indexSet, withAnimation:.effectFade)
+            
+            if analyses.isEmpty {
+                let analysis = Analysis(name: getUniqueAnalysisName(prefix: "untitled analysis", isCopy: false))
+                addAnalysis(analysis, row: 0)
+            } else {
+                if let minIndex = indexSet.min() {
+                    let selectionIndex = minIndex == 0 ? 0 : minIndex - 1
+                    let previousIndexSet = IndexSet(integer: selectionIndex)
+                    analysesTableView.selectRowIndexes(previousIndexSet, byExtendingSelection: false)
+                }
+            }
+            
+        default: break
+            
         }
     }
 }
