f3d1d641db22de88a8e23ddaa292e5489a34c45a
Sveta Krasikova <svetakrasikova@me.com>
2020-05-01 09:22:13 -0700

    Add new json mock ups for distributions.

--- macgui/TestDataConstants.swift
+++ macgui/TestDataConstants.swift
@@ -10,6 +10,9 @@ import Foundation
 
 class TestDataConstants {
     
+    
+//    MARK: -- Data Matrix
+    
     static  let emptyBitVectorJsonString: String = String(data: (try! JSONEncoder().encode(Bitvector())), encoding: .utf8)!
     
     static let  (A, T, G, C): (String, String, String, String) = ("A", "T", "G", "C")
@@ -41,5 +44,50 @@ class TestDataConstants {
      }
  }
 """.data(using: .utf8)!
+    
+//    MARK: -- Model Tool
+    
+    static let exponentialDistribution = """
+            {
+                "name": "Exponential Distribution"
+                "domain": "PosReal"
+                "descriptiveString": "The exponential distribution models the waiting time between events that occur at a constant rate, \u{039B}."
+                "parameters": [{"name": "\u{039B}", "type": "PosReal", "descriptiveString": "The rate parameter of the process"}]
 
+            }
+"""
+    static let normalDistribution =  """
+            {
+                "name": "Normal Distribution"
+                "domain": "Real"
+                "descriptiveString": "The normal distribution, also known as the Gaussian or Bell-shaped distribution, is used to model many biological phenomena, partly because of the Central Limit Theorem."
+                "parameters": [{"name": "\u{03BC}", "type": "Real", "descriptiveString": "The mean of the normal distribution"},
+                                {"name": "\u{03C3}", "type": "PosReal", "descriptiveString": "The standard deviation of the distribution."}
+                                ]
+
+            }
+"""
+    
+    static let gammaDistribution =  """
+                {
+                    "name": "Gamma Distribution"
+                    "domain": "PosReal"
+                    "descriptiveString": "The gamma distribution represents a generalization of the exponential, with the rate being the scale parameter and the number of summed sojourn times being the scale."
+                    "parameters": [{"name": "\u{03B1}", "type": "PosReal", "descriptiveString": "The shape parameter of the gamma distribution."},
+                                    {"name": "\u{03D0}", "type": "PosReal", "descriptiveString": "The scale parameter of the gamma distribution."}
+                                    ]
+
+                }
+    """
+    
+    
+    static let poissonDistribution =  """
+                {
+                    "name": "Poisson Distribution"
+                    "domain": "Natural"
+                    "descriptiveString": "The Poisson distribution models the number of events that occur in some time interval when the rate at which the events occur is constant."
+                    "parameters": [{"name": "\u{039B}", "type": "PosReal", "descriptiveString": "The rate parameter of the process."}]
+
+                }
+    """
 }
