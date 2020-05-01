af97c2c7b74b857f9e38f0886832d188304ae13a
Sveta Krasikova <svetakrasikova@me.com>
2020-05-01 09:22:33 -0700

    Add some green letter symbols.

--- macgui/Extensions/Symbol.swift
+++ macgui/Extensions/Symbol.swift
@@ -14,6 +14,11 @@ enum Symbol: String {
     case doubleStruckCapitalP = "\u{2119}"
     case doubleStruckCapitalQ = "\u{211A}"
     case doubleStruckCapitalZ = "\u{2124}"
-    case capitalDelta         = "\u{0394}"
-    case plus                 = "\u{002B}"
+    case capitalDelta = "\u{0394}"
+    case plus = "\u{002B}"
+    case sigma = "\u{03C3}"
+    case mu = "\u{03BC}"
+    case lambda = "\u{039B}"
+    case alpha = "\u{03B1}"
+    case beta = "\u{03D0}"
 }
