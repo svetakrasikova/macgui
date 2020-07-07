18832278066b311cf404e5992ecd4026cc098225
Sveta Krasikova <svetakrasikova@me.com>
2020-07-07 11:56:49 -0700

    Add clustal omega options.

---
+++ macgui/Binaries/Clustal/ClustalOmegaOptions.swift
@@ -0,0 +1,33 @@
+//
+//  ClustalOmegaOptions.swift
+//  macgui
+//
+//  Created by Svetlana Krasikova on 7/7/20.
+//  Copyright © 2020 Svetlana Krasikova. All rights reserved.
+//
+
+import Cocoa
+
+class ClustalOmegaOptions: NSObject, NSCoding {
+    
+    // MARK: - Coding Keys
+    
+    private enum CodingKeys: String, CodingKey {
+        
+        case dealign
+        case 
+    }
+    
+ 
+//    MARK: - NSCoding
+    
+    func encode(with coder: NSCoder) {
+        <#code#>
+    }
+    
+    required init?(coder: NSCoder) {
+        <#code#>
+    }
+    
+
+}
