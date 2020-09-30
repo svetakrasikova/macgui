05711b0fc445a92a781c28d73cc3038f7b52e3ab
Sveta Krasikova <svetakrasikova@me.com>
2020-09-30 15:07:36 -0700

    Indentation.

--- macgui/Views and View Controllers/Document/Document.swift
+++ macgui/Views and View Controllers/Document/Document.swift
@@ -11,12 +11,12 @@ import Cocoa
 class Document: NSDocument {
     
     @objc dynamic var dataSource: DataSource? = DataSource()
-   
+    
     override init() {
         super.init()
         NotificationCenter.default.addObserver(self, selector: #selector(didUpdateDocument(notification:)), name: .didUpdateDocument, object: nil)
     }
-
+    
     override class var autosavesInPlace: Bool {
         return true
     }
@@ -24,24 +24,25 @@ class Document: NSDocument {
     @objc func didUpdateDocument(notification: Notification) {
         self.updateChangeCount(.changeDone)
     }
-
+    
     override func makeWindowControllers() {
         // Returns the Storyboard that contains your Document window.
         let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
         let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document Window Controller")) as! NSWindowController
         self.addWindowController(windowController)
     }
-
+    
     override func data(ofType typeName: String) throws -> Data {
         if let dataSource = self.dataSource {
-                  return NSKeyedArchiver.archivedData(withRootObject: dataSource)
-              } else {
-                  throw NSError(domain: "Document", code: 0, userInfo: nil)
-              }
+            return NSKeyedArchiver.archivedData(withRootObject: dataSource)
+        } else {
+            throw NSError(domain: "Document", code: 0, userInfo: nil)
+        }
     }
-
+    
     override func read(from data: Data, ofType typeName: String) throws {
-       if let dataSource = NSKeyedUnarchiver.unarchiveObject(with: data) as? DataSource {
+        
+        if let dataSource = NSKeyedUnarchiver.unarchiveObject(with: data) as? DataSource {
             self.dataSource = dataSource
         } else {
             throw NSError(domain: "Document", code: 0, userInfo: nil)
@@ -49,7 +50,6 @@ class Document: NSDocument {
     }
     
     
-
-
+    
 }
 
