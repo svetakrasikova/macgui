807184348ea695cc42de4d7c7f9e7f1c34205942
Sveta Krasikova <svetakrasikova@me.com>
2020-11-03 11:21:07 -0800

    Implement rooting a tree with a specified outgroup.

--- macgui/Model/Tree/Node.swift
+++ macgui/Model/Tree/Node.swift
@@ -1,31 +1,37 @@
 import Cocoa
 
 
+/**
+ An object that models a node of an evolutionary tree. It has an optional ancestor and zero, or two and more children. The name property is otional. It holds the taxon name for leaf nodes.
+ */
 
 class Node: NSObject, NSCoding {
     
-
-    var descendants : [Node] = []
-    var ancestor : Node?
+    
+    var descendants: [Node] = []
+    var ancestor: Node?
     var index: Int = 0
     var branchLength: Double = 0.0
     var name: String = ""
+    var depthFromTip: Int = 0
     
     var isLeaf: Bool {
         return descendants.isEmpty
     }
+    
     var isRoot: Bool {
         return ancestor == nil
     }
     
-    var depthFromTip: Int = 0
-    
     var x: CGFloat {
         point.x
     }
+    
     var y: CGFloat {
         point.y
     }
+    
+    /// The coordinates of the node for displaying in UI
     
     var point: NSPoint = NSPoint(x: 0.0, y: 0.0)
     
@@ -48,7 +54,7 @@ class Node: NSObject, NSCoding {
         coder.encode(name, forKey: Key.name.rawValue)
         coder.encode(point, forKey: Key.point.rawValue)
         coder.encode(depthFromTip, forKey: Key.depthFromTip.rawValue)
-    
+        
     }
     
     required init?(coder: NSCoder) {
@@ -71,10 +77,9 @@ class Node: NSObject, NSCoding {
         return depth
     }
     
-    // MARK: -
-
+    
     override var description: String {
-
+        
         var str: String = ""
         str += "Node\n"
         str += "   Name  = \"\(name)\"\n"
@@ -89,41 +94,71 @@ class Node: NSObject, NSCoding {
         return str
     }
     
-    func passDown(node: Node?, tSeq: inout [Node]) {
-        
-        if let node = node {
-            
-            for d in node.descendants {
-                passDown(node: d, tSeq: &tSeq)
-            }
-            tSeq.append(node)
+    /**
+     Perfroms a depth first traversal of the tree rooted at the given node.
+     
+     - Returns: an array of all descendant and self, in the post-order
+     
+     */
+    func passDown() -> [Node] {
+        var tSeq: [Node] = []
+        for d in descendants {
+            tSeq += d.passDown()
         }
-    }
-    
-    func passDown(node: Node?, tSeq: [Node]) -> [Node] {
-        
-        if let node = node {
-            var updatedTSeq: [Node] = []
-            for d in node.descendants {
-                updatedTSeq += passDown(node: d, tSeq: tSeq)
-            }
-            updatedTSeq.append(node)
-            return updatedTSeq
-        }
+        tSeq.append(self)
         return tSeq
     }
     
-    func show(node: Node?, indent: Int) {
+    /**
+    Traverses the tree rooted at the given node recursively and creates a copy of the node with the given leaf removed.
+     
+     - Parameter taxonName: the name of the leaf to be removed
+     - Returns: A copy of the node with the leaf node removed; nil if the node is the leaf to be removed
+        
+     */
     
+    func removeLeafWith(taxonName: String) throws -> Node? {
+        
+        let newNode: Node = self
+        
+        switch newNode.descendants.count {
+        case 0:
+            if newNode.name == taxonName { return nil }
+            else { return newNode }
+        case 1:
+            throw Tree.TreeError.unaryBranch
+        default:
+            for (index, node) in newNode.descendants.enumerated() {
+                if node.name == taxonName {
+                    newNode.descendants.remove(at: index)
+                    if newNode.descendants.count == 1 {
+                        let leaf = newNode.descendants.first!
+                        leaf.ancestor = newNode.ancestor
+                        return leaf
+                    }
+                }
+                else if node.descendants.count > 0 {
+                    if let replaceNode = try node.removeLeafWith(taxonName: taxonName) {
+                        newNode.descendants[index] = replaceNode
+                    }
+                }
+            }
+        }
+        return newNode
+    }
+    
+    
+    func show(node: Node?, indent: Int) {
+        
         if let node = node {
-
+            
             var str : String = ""
             for _ in 0...indent {
                 str += " "
             }
             str += "\(node.index) -- "
             str += node.ancestor == nil ? "nil ( " : "\(node.ancestor!.index) ( "
- 
+            
             for d in node.descendants {
                 str += "\(d.index) "
             }
--- macgui/Model/Tree/Tree.swift
+++ macgui/Model/Tree/Tree.swift
@@ -3,89 +3,47 @@ import Cocoa
 
 class Tree: NSObject, NSCoding {
     
-
-    var nodes: [Node] = []
-    var root: Node?
+    var root: Node
+    var tSequence : [Node] {
+        return root.passDown()
+    }
     
-    
-    var tSequence: [Node] {
-        return root?.passDown(node: root, tSeq: []) ?? []
-
+    enum TreeError: Error {
+        
+        case badNewickString
+        case unaryBranch
     }
     
     var numberOfTaxa: Int {
-        (self.nodes.filter { $0.isLeaf }).count
+        (self.tSequence.filter { $0.isLeaf }).count
     }
     
     var nodeCoordinates: [NSPoint] = []
-
-     enum TreeError: Error {
-     
-         case badNewickString
-    }
+    
     
     enum Key: String {
-        case nodes, root, nodeCoordinates
+        case root, nodeCoordinates
     }
     
     
     func encode(with coder: NSCoder) {
-        coder.encode(nodes, forKey: Key.nodes.rawValue)
         coder.encode(root, forKey: Key.root.rawValue)
         coder.encode(nodeCoordinates, forKey: Key.nodeCoordinates.rawValue)
     }
     
     required init?(coder: NSCoder) {
-        nodes = coder.decodeObject(forKey: Key.nodes.rawValue) as? [Node] ?? []
-        root = coder.decodeObject(forKey: Key.nodes.rawValue) as? Node
+        root = coder.decodeObject(forKey: Key.root.rawValue) as! Node
         nodeCoordinates = coder.decodeObject(forKey: Key.nodeCoordinates.rawValue) as? [NSPoint] ?? []
     }
     
-    init(root: Node, nodes: [Node]) {
+    init(root: Node) {
         self.root = root
-        self.nodes = nodes
     }
     
-    func rooted() -> Tree {
-        
-        if root == nil {
-            var rootedTreeNodes: [Node] = []
-            var root: Node = Node()
-            rootedTreeNodes.append(root)
-            
-            let nodesWithoutAncestor: [Node] = self.nodes.filter { $0.ancestor == nil }
-            let nodesWithAncestor: [Node] = self.nodes.filter { $0.ancestor != nil }
-            if nodesWithoutAncestor.count == 1 {
-                root = nodesWithoutAncestor.first!
-                rootedTreeNodes.append(contentsOf: nodesWithAncestor)
-            } else {
-                var outgroup: Node = Node()
-                let ingroup: Node = Node()
-                ingroup.ancestor = root
-                outgroup.ancestor = root
-                var addedOutgroup = false
-                for node in self.nodes {
-                    rootedTreeNodes.append(node)
-                    if node.ancestor == nil, !addedOutgroup{
-                        outgroup = node
-                        root.descendants.append(outgroup)
-                        addedOutgroup = true
-                        
-                    } else if node.ancestor == nil {
-                        ingroup.descendants.append(node)
-                    }
-                }
-                root.descendants.append(ingroup)
-                rootedTreeNodes.append(ingroup)
-            }
-            return Tree(root: root, nodes: rootedTreeNodes)
-            
-        } else { return self }
-    }
     
-//    MARK: - Coordinates
+    //    MARK: - Coordinates
     
-    func setCoordinates(drawMonophyleticWrOutgroup: Bool) {
+    func setCoordinates() {
         
         guard !tSequence.isEmpty else { return }
         
@@ -98,11 +56,9 @@ class Tree: NSObject, NSCoding {
             }
             
         }
-        guard var depthOfRoot = root?.depthFromTip else { return }
         
-        if drawMonophyleticWrOutgroup {
-            depthOfRoot += 1
-        }
+        let depthOfRoot = root.depthFromTip
+        
         
         for node in tSequence {
             node.point.y = 1.0 - (CGFloat(node.depthFromTip)/CGFloat(depthOfRoot))
@@ -125,10 +81,10 @@ class Tree: NSObject, NSCoding {
                 var xMin: CGFloat = 1000000000.0;
                 var xMax: CGFloat = 0.0;
                 
-                for (i,d) in node.descendants.enumerated(){
-                    if i == 0, node === root, drawMonophyleticWrOutgroup {
-                        continue
-                    }
+                for d in node.descendants {
+//                    if i == 0, node === root, drawMonophyleticWrOutgroup {
+//                        continue
+//                    }
                     if d.point.x < xMin { xMin = d.point.x }
                     else if d.point.x > xMax { xMax = d.point.x }
                     
@@ -150,68 +106,39 @@ class Tree: NSObject, NSCoding {
         
     }
     
+    //    MARK: - Tree from Newick String
     
-    
-    
-//    MARK: - Tree from Newick String
-    
-
     init(newickString: String) throws {
         
-        super.init()
-        
-        
         do {
-            
             let newick = try NewickString(nStr: newickString)
-            treeFromNewick(newickTokens: newick.newickTokens)
+            self.root = Tree.treeFromNewick(newickTokens: newick.newickTokens)
             
         } catch {
             throw TreeError.badNewickString
         }
         
-        
+        super.init()
     }
     
-    override var description: String {
-
-        var str: String = ""
-        str += "Tree\n"
-        str += "   Number of nodes = \(nodes.count)\n"
-        str += "   Root = \(String(describing: root?.index))\n"
-        str += "   Postorder = ( "
-        for n in tSequence {
-            str += "\(String(describing: n.index)) "
-        }
-        str += ")\n"
-        
-        return str
-    }
-    
-    func showTree() {
-        
-        root?.show(node: root!, indent: 0)
-    }
-    
-    func treeFromNewick(newickTokens: [String]) {
-
+    class func treeFromNewick(newickTokens: [String]) -> Node {
+        var firstNode = true
+        var root = Node()
         var p : Node? = nil
         var readingBrlen : Bool = false
         var idx : Int = 0
         for token in newickTokens {
-        
             if token == "(" {
-                if nodes.count == 0 {
+                if firstNode {
                     p = Node()
                     p?.index = idx
                     idx += 1
-                    root = p
-                    nodes.append(p!)
+                    root = p!
+                    firstNode = false
                 } else {
                     let q = Node()
                     q.index = idx
                     idx += 1
-                    nodes.append(q)
                     q.ancestor = p
                     p?.descendants.append(q)
                     p = q
@@ -226,14 +153,13 @@ class Tree: NSObject, NSCoding {
             } else if token == ":" {
                 readingBrlen = true
             } else if token == ";" {
-            
+                
             } else {
                 if readingBrlen == false {
                     let q = Node()
                     q.index = idx
                     idx += 1
                     q.name = String(token)
-                    nodes.append(q)
                     q.ancestor = p
                     p?.descendants.append(q)
                     p = q
@@ -243,7 +169,58 @@ class Tree: NSObject, NSCoding {
                 }
             }
         }
-
+        return root
     }
-
+    
+    
+//    MARK: - Printing Tree
+    
+    override var description: String {
+        
+        let nodes = tSequence
+        var str: String = ""
+        str += "Tree\n"
+        str += "   Number of nodes = \(nodes.count)\n"
+        str += "   Root = \(String(describing: root.index))\n"
+        str += "   Postorder = ( "
+        for n in tSequence {
+            str += "\(String(describing: n.index)) "
+        }
+        str += ")\n"
+        for n in nodes {
+            print(n)
+        }
+        return str
+    }
+    
+    func showTree() {
+        root.show(node: root, indent: 0)
+    }
+    
+//    MARK: - Rooting Tree
+    
+/**
+      Creates a copy of the tree  with the birfurcating (binary) root, using the outgroup method.
+     - Parameter taxon: The name of the leaf node to serve as the outgroup
+     - Returns: A tree identical to the input tree with the possible exception of the root node that is obligatorily binary branching.
+     - Remark: If the input tree is binary at the root, its copy is returned.
+     
+     */
+    
+    func rootWithOutgroup(_ taxon: String) throws -> Tree {
+        
+        guard self.root.descendants.count > 2 else { return self }
+        
+        let outgroup = Node()
+        outgroup.name = taxon
+        let newRoot = Node()
+        newRoot.descendants = [outgroup]
+        
+        if let ingroup = try! root.removeLeafWith(taxonName: taxon) {
+            newRoot.descendants.append(contentsOf: ingroup.passDown())
+        }
+        
+        return Tree(root: newRoot)
+    }
+    
 }
