073ad2ad03e10084cf18f52a05661bc3b849bc3a
Sveta Krasikova <svetakrasikova@me.com>
2020-11-29 14:19:40 -0800

    Remove white background from summary view.

--- macgui/Views and View Controllers/Canvas Object/Canvas Tool/Parsimony Tool/SummaryViewConroller.swift
+++ macgui/Views and View Controllers/Canvas Object/Canvas Tool/Parsimony Tool/SummaryViewConroller.swift
@@ -27,8 +27,6 @@ class SummaryViewConroller: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.wantsLayer = true
-        view.layer?.backgroundColor = NSColor.white.cgColor
         
     }
     
