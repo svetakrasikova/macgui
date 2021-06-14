87b5437e81d83cf53f1e6adc64df50158c9470a2
Svetlana Krasikova <svetakrasikova@me.com>
2021-06-13 17:28:27 -0700

    Model Plate Controller help text field.

--- macgui/Views and View Controllers/Canvas Object/Canvas Tool/Model Tool/ModelPlateController.swift
+++ macgui/Views and View Controllers/Canvas Object/Canvas Tool/Model Tool/ModelPlateController.swift
@@ -9,11 +9,10 @@
 import Cocoa
 
 class ModelPlateController: LoopController {
- 
-//    todo: set the range popup when the view is presented
-//    disable the menu items in the popup that are not available if there are no matrix data or if the loop is not embedded
-//     set the help text and update it on select range
-//      update help and toggle value and update range selection if matrices are removed from the model tool
+
+//TODO:
+// update help and toggle value and update range selection if matrices are removed from the model tool
+// check setting up of the index popup
     
     
     @IBOutlet weak var box: NSBox!
@@ -22,15 +21,41 @@ class ModelPlateController: LoopController {
     @IBOutlet weak var valueStack: NSStackView!
     
     @IBOutlet weak var helpField: NSTextField!
+    
     var plate: Plate? {
         self.loop as? Plate
     }
     
+    private var observers =  [NSKeyValueObservation]()
+    
+    func setDataObserver(){
+        guard let plate = self.plate else { return }
+        self.observers = [
+            plate.observe(\Plate.upperRange, options: [.initial]) {(plate, _) in
+                self.setHelpText()
+                self.view.needsDisplay = true
+            }
+        ]
+    }
+    
+    @IBOutlet weak var rangePopup: NSPopUpButton!
+    
     var matricesCount: Int?
-
+    
+    var isEmbeddedInOuterLoopWithMatrixRange: Bool {
+        guard let plate = self.plate else { return false }
+        if let outerPlate = plate.outerLoop as? Plate {
+            return outerPlate.rangeType == Plate.IteratorRange.numberMatrices.rawValue
+        }
+        return false
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setDataObserver()
+        setRangePopup()
+        setHelpText()
     }
     
     
@@ -40,78 +65,106 @@ class ModelPlateController: LoopController {
         preferredContentSize =  NSSize(width: 450, height: fittingSize.height)
     }
     
-//    func setUpIndexPopup() {
-//        indexPopup.removeAllItems()
-//        indexPopup.addItems(withTitles: delegate?.allIndices() ?? [])
-//        setDataForIndexpopup()
-//    }
-//
-//    func setDataForIndexpopup() {
-//        guard let delegate = self.delegate else { return }
-//        let all = delegate.allIndices()
-//        let active = delegate.activeIndices()
-//        if let index = loop?.index, let loopIndex = all.firstIndex(of: index) {
-//            indexPopup.selectItem(at: loopIndex)
-//            for title in active {
-//                indexPopup.item(withTitle: title)?.isEnabled = title != index ? false : true
-//            }
-//        }
-//    }
-    
-    func setHelpText(_ rangeType: Int) {
+    override func viewDidAppear() {
+        super.viewDidAppear()
         guard let plate = self.plate else { return }
-        let isEmbedded: Bool = plate.outerLoop != nil
-        var index: String?
-        var isEmbeddedInOuterLoopWithMatrixRange = false
-        if let outerPlate = plate.outerLoop as? Plate {
-            isEmbeddedInOuterLoopWithMatrixRange = outerPlate.rangeType == Plate.IteratorRange.numberMatrices.rawValue
-            index = outerPlate.index
-        }
-        guard let matricesCount = self.matricesCount else { return }
-        switch rangeType {
-        case Plate.IteratorRange.number.rawValue:
-            helpField.stringValue = plate.upperRange == 1 ?
-                "This plate iterates over a range of one. Consider changing the range for this plate, or simply deleting the plate."
-                :
-                "This plate iterates over a range of \(plate.upperRange)."
-        case Plate.IteratorRange.numberMatrices.rawValue:
-            var str = "This plate will range over the number of character matrices attached to the Model Tool. "
-            str = matricesCount > 0 ?
-                str + "Currently, the Model Tool contains \(matricesCount) matrix/ces."
-                :
-                str + "Currently, the Model Tool does not have data matrices attached to it."
-            helpField.stringValue = str
-        case Plate.IteratorRange.numberChar.rawValue:
-            var str = "This plate will range over the number of characters in a character matrix attached to the Model Tool. "
-            if matricesCount == 0 {
-                str += "Currently, the Model Tool does not have data matrices attached to it."
-            } else {
-                str = !isEmbedded ? str + "Currently, this plate is not contained by any plates that could be configured to range over character matrices."
-                    :
-                    helpTextForEmbeddedPlates(isEmbeddedInOuterLoopWithMatrixRange, str: str, index: index)
+        setValue(index: plate.rangeType)
+    }
+    
+    func setRangePopup() {
+        guard let plate = self.plate else { return }
+        let selectedIndex = plate.rangeType
+        if let numMatrices = self.matricesCount {
+            if numMatrices > 0 {
+                if isEmbeddedInOuterLoopWithMatrixRange {
+                    for i in 0..<rangePopup.numberOfItems {
+                        let isNumChar: Bool = i == Plate.IteratorRange.numberChar.rawValue
+                        let isNumTaxa: Bool = i == Plate.IteratorRange.numberTaxa.rawValue
+                        rangePopup.item(at: i)?.isEnabled = isNumChar || isNumTaxa ? true : false
+                    }
+                } else {
+                    for i in 0..<rangePopup.numberOfItems {
+                        let isNum: Bool = i == Plate.IteratorRange.number.rawValue
+                        let isNumMatrices: Bool = i == Plate.IteratorRange.numberMatrices.rawValue
+                        rangePopup.item(at: i)?.isEnabled = isNum || isNumMatrices ? true : false
+                    }
+                }
                 
+            } else {
+                for i in 0..<rangePopup.numberOfItems {
+                    let isNum: Bool = i == Plate.IteratorRange.number.rawValue
+                    rangePopup.item(at: i)?.isEnabled = isNum ? true : false
+                }
             }
-            helpField.stringValue = str
-        default: break
+            
+            if rangePopup.item(at: selectedIndex)!.isEnabled {
+                rangePopup.selectItem(at: selectedIndex)
+            } else {  print("Error in setRangePopup") }
+            
+            
         }
     }
     
-    func helpTextForEmbeddedPlates(_ isEmbeddedInOuterLoopWithMatrixRange: Bool, str: String, index: String?) -> String {
-        guard isEmbeddedInOuterLoopWithMatrixRange, let index = index else { return "" }
-        let text = !isEmbeddedInOuterLoopWithMatrixRange ?
-            str + "Currently, this plate is not contained by a plate that ranges over character matrices."
+    func setHelpText() {
+        guard let plate = self.plate else { return }
+        var str = ""
+        switch plate.rangeType {
+        case Plate.IteratorRange.number.rawValue:
+            str = helpTextForNumPlates()
+        case Plate.IteratorRange.numberMatrices.rawValue:
+            str = helpTextForNumMatricesPlates()
+        case Plate.IteratorRange.numberChar.rawValue:
+            str = helpTextForEmbeddedCharPlates(index: plate.outerLoop?.index)
+        case Plate.IteratorRange.numberTaxa.rawValue:
+            str = helpTextForEmbeddedTaxaPlates(index: plate.outerLoop?.index)
+        default: break
+        }
+        helpField.stringValue = str
+    }
+    
+    func helpTextForNumPlates() -> String {
+        guard let plate = self.plate else { return "" }
+        return plate.upperRange == 1 ?
+            "This plate iterates over a range of one. Consider changing the range for this plate, or simply deleting the plate."
             :
-            "This plate ranges over the number of characters in the \(index)-th character matrix attached to the Model Tool."
+            "This plate iterates over a range of \(plate.upperRange)."
         
-        return text
+    }
+    
+    func helpTextForNumMatricesPlates() -> String {
+        guard let matricesCount = self.matricesCount
+        else {
+            return ""
+        }
+        let numMatricesStr = matricesCount == 1 ? "\(matricesCount) matrix" : "\(matricesCount) matrices"
+        return "This plate will range over the number of character matrices attached to the Model Tool. Currently, the Model Tool contains \(numMatricesStr)."
         
+    }
+    
+    func helpTextForEmbeddedCharPlates(index: String?) -> String {
+        guard let index = index
+        else {
+            return ""
+        }
+        return "This plate ranges over the number of characters in the \(index)-th character matrix attached to the Model Tool."
+        
+    }
+    
+    func helpTextForEmbeddedTaxaPlates(index: String?) -> String {
+        guard let index = index else { return "" }
+        return "This plate ranges over the number of taxa in the \(index)-th character matrix attached to the Model Tool."
     }
     
     
     
     @IBAction func selectRange(_ sender: NSPopUpButton) {
+        setValue(index: sender.indexOfSelectedItem)
+        setHelpText()
+    }
+    
+    func setValue(index: Int) {
         box.removeConstraint(boxHeightConstraint)
-        let hideValue = sender.indexOfSelectedItem != Plate.IteratorRange.number.rawValue
+        let hideValue = index != Plate.IteratorRange.number.rawValue
         toggleValue(hide: hideValue)
         addHeightConstraintToBox()
     }
@@ -122,14 +175,14 @@ class ModelPlateController: LoopController {
     }
     
     func addHeightConstraintToBox() {
-
+        
         boxHeightConstraint    = NSLayoutConstraint(item: box as Any,
-                                                 attribute: NSLayoutConstraint.Attribute.height,
-                                                 relatedBy: NSLayoutConstraint.Relation.equal,
-                                                 toItem: nil,
-                                                 attribute: NSLayoutConstraint.Attribute.notAnAttribute,
-                                                 multiplier: 1,
-                                                 constant: box.fittingSize.height)
+                                                    attribute: NSLayoutConstraint.Attribute.height,
+                                                    relatedBy: NSLayoutConstraint.Relation.equal,
+                                                    toItem: nil,
+                                                    attribute: NSLayoutConstraint.Attribute.notAnAttribute,
+                                                    multiplier: 1,
+                                                    constant: box.fittingSize.height)
         box.addConstraint(boxHeightConstraint)
     }
     
